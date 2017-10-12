55ca8d07293244d5de73e9867dbfff469708cb61
Gabe Maiberger <gabrielmaiberger@gmail.com>
2017-10-12 15:57:37 -0600

    Added LGPL notices to code

--- include/r3d.h
+++ include/r3d.h
@@ -1,3 +1,21 @@
+/*
+R3D Cipher
+Copyright (C) 2017 Gabriel Nathan Maiberger
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 void r3d_encrypt_block(unsigned char plaintext_block[512], unsigned char key[512], unsigned char ciphertext_block[512]);
 void r3d_decrypt_block(unsigned char ciphertext_block[512], unsigned char key[512], unsigned char plaintext_block[512]);
 
