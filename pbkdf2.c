6df55e90a457ef71837d770c6501132f6f9a99ef
Gabe Maiberger <gabrielmaiberger@gmail.com>
2017-10-23 21:27:20 -0600

    Improve SHA-3 and PBKDF2 permutations

--- pbkdf2.c
+++ pbkdf2.c
@@ -26,13 +26,16 @@ unsigned char *pbkdf2_function(unsigned char *password, unsigned char *salt, int
 unsigned char *hmac_sha3(unsigned char *key, unsigned char *message);
 
 unsigned char *pbkdf2_derive_key(unsigned char *password, unsigned char *salt, int c, int len){
-	unsigned char *t[len/64];
+	int n=len/64;
+
+	printf("%i\n", n);
+
+	unsigned char *t[n];
 	unsigned char *key=malloc(len);
 
 	int i;
-	for(i=0; i<len/64; i++){
+	for(i=0; i<n; i++){
 		t[i]=pbkdf2_function(password, salt, c, i);
-		//t[i]=hmac_sha3(key,salt);
 		memset(key+i*64, (long int)t[i], 64);
 	}
 
@@ -46,16 +49,11 @@ unsigned char *pbkdf2_function(unsigned char *password, unsigned char *salt, int
 	int j;
 	for(j=0; j<c; j++){
 		if(j==0){
-			//strcat(salt, (unsigned char *)&i);
+			strcat(salt, (unsigned char *)&i);
 			u[j]=hmac_sha3(password, salt);
-		} else {
-			u[j]=hmac_sha3(password, u[j-1]);
-		}
-	}
-	for(j=0; j<c; j++){
-		if(j==0){
 			u_final=(long int)u[j];
 		} else {
+			u[j]=hmac_sha3(password, u[j-1]);
 			u_final^=(long int)u[j];
 		}
 	}
@@ -65,14 +63,18 @@ unsigned char *pbkdf2_function(unsigned char *password, unsigned char *salt, int
 
 unsigned char *hmac_sha3(unsigned char *key, unsigned char *message){
 	int size=sizeof(key)/sizeof(key[0]);
+	printf("%i\n", size);
+
 	if(size>64){
 		key=sha3_512(key,64);
+		size=64;
 	} else if(size<64){
 		strncat(key, (unsigned char *)"\x00", 64-size);
+		size=64;
 	}
 
-	unsigned char *o_key_pad=malloc(64);
-	unsigned char *i_key_pad=malloc(64);
+	unsigned char *o_key_pad=malloc(128);
+	unsigned char *i_key_pad=malloc(128);
 
 	int i;
 	for(i=0; i<64; i++){
@@ -80,14 +82,11 @@ unsigned char *hmac_sha3(unsigned char *key, unsigned char *message){
 		memset((i_key_pad+i), (long int)(0x36*64)^(long int)key[i], 1);
 	}
 
-	unsigned char *i_key_pad_hash=malloc(64);
-
 	strcat(i_key_pad, message);
-	i_key_pad_hash=sha3_512(i_key_pad, strlen(i_key_pad));
+	unsigned char *i_key_pad_hash=sha3_512(i_key_pad, 128);
 	strcat(o_key_pad, i_key_pad_hash);
 
-	unsigned char *hash=malloc(64);
-	hash=sha3_512(o_key_pad, strlen(o_key_pad));
+	unsigned char *hash=sha3_512(o_key_pad, 128);
 
 	return hash;
 }
--- sha3.c
+++ sha3.c
@@ -18,25 +18,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string.h>
 #include <malloc.h>
+#include <math.h>
+
+unsigned long long int RC[24]={
+0x0000000000000001, 0x0000000000008082,
+0x800000000000808A, 0x8000000080008000,
+0x000000000000808B, 0x0000000080000001,
+0x8000000080008081, 0x8000000000008009,
+0x000000000000008A, 0x0000000000000088,
+0x0000000080008009, 0x000000008000000A,
+0x000000008000808B, 0x800000000000008B,
+0x8000000000008089, 0x8000000000008003,
+0x8000000000008002, 0x8000000000000080,
+0x000000000000800A, 0x800000008000000A,
+0x8000000080008081, 0x8000000000008080,
+0x0000000080000001, 0x8000000080008008
+};
 
 unsigned char *sha3_512(unsigned char *data, int size);
 unsigned char *sha3_pad(unsigned char *data);
 unsigned char *sha3_permute_block(unsigned char *data);
+unsigned int parity(unsigned int n);
 
 unsigned char *sha3_512(unsigned char *data, int size){
 	unsigned char *P;
-	
-	if(size%72!=0){
+
+ 	if(size%72!=0){
 		P=sha3_pad(data);
 		size+=(size%72);
 	}
 
+	printf("%i\n", size);
+
 	int n=(size/72);
+
+	printf("%i\n", n);
 
 	unsigned char *p[n];
 
 	int i;
-	for(i=0; i<n; i++){
+	int j;
+	for(i=0; i<=n; i++){
 		p[i]=malloc(72);
 		strncpy(p[i], P+(i*72), 72);
 	}
@@ -44,14 +66,16 @@ unsigned char *sha3_512(unsigned char *data, int size){
 	unsigned char *S=malloc(72);
 	bzero(S, 72);
 
-	int c=6;
+	int c=128;
 	for(i=0; i<n; i++){
-		strncat(p[i], "\0", 6);
-		S[i]^=(long int)p[i];
+		strncat(p[i], "\0", c);
+		for(j=0; j<72; j++){
+			S[j%72]^=p[i][j];
+		}
 		sha3_permute_block(S);
 	}
 
-	unsigned char *Z=malloc(64);
+	unsigned char *Z=malloc(72);
 
 	strncpy(Z, S, 64);
 
@@ -60,26 +84,26 @@ unsigned char *sha3_512(unsigned char *data, int size){
 
 unsigned char *sha3_pad(unsigned char *data){
 	int i;
-	long int size=sizeof(data)/sizeof(data[0]);
+	int size=sizeof(data)/sizeof(data[0]);
+	int margin=(72-size%72);
 
-	for(i=0; i<(72-size); i++){
-		if(i==0){
-			strcat(data, "\1");
-		} else {
-			size=sizeof(data)/sizeof(data[0]);
-			if(i==((72-size)-1)){
-				strcat(data, "\xff");
-			} else {
-				strcat(data, "\x00");
-			}
-		}
+	printf("%i\n", margin);
+
+	unsigned char *P=malloc(size+margin);
+
+	memcpy(P, data, size);
+
+	P[size]=0x80;
+	for(i=1; i<margin; i++){
+		P[size+i]=0;
 	}
+	P[size+margin-1]=0x02;
 
-	return data;
+	return P;
 }
 
 unsigned char *sha3_permute_block(unsigned char *data){
-	unsigned char state[5][5][6];
+	unsigned char state[5][5][8];
 
 	int i;
 	int j;
@@ -88,60 +112,73 @@ unsigned char *sha3_permute_block(unsigned char *data){
 
 	for(i=0; i<5; i++){
 		for(j=0; j<5; j++){
-			for(k=0; k<6; k++){
-				state[i][j][k]=data[(5*i+j)*6+k];
+			for(k=0; k<8; k++){
+				state[i][j][k]=data[(i*5+j)*8+k];
 			}
 		}
 	}
 	
 	for(r=0; r<24; r++){
-		for(i=0; i<4; i++){
+		for(i=0; i<5; i++){
 			for(j=0; j<5; j++){
-				for(k=0; k<6; k++){
-					state[i][j][k]^=state[i][j-1][k]^state[i][j+1][k-1];
+				for(k=0; k<8; k++){
+					state[i][j][k]^=(parity(state[i][j-1][k])^parity(state[i][j+1][k-1]));
 				}
 			}
 		}
 
-		/*int t=0;
+		int t=0;
 		int t_inc=1;
-		for(i=1; i<5; i++){
-			for(j=1; j<5; j++){
-				for(k=0; k<6; k++){
-					if(t>24){
-						t=0;
-						t_inc=1;
-					}
-					state[i][j][k]=state[i][j][k-(t+1)*(t+2)/2];
+		k=0;
+		for(i=0; i<5; i++){
+			if(i==0){
+				for(j=1; j<5; j++){
+					state[i][j][k]=state[i][j][k-((t+1)*(t+2)/2)];
+					t_inc++;
+					t+=t_inc;
+				}
+			} else {
+				for(j=0; j<5; j++){
+					state[i][j][k]=state[i][j][k-((t+1)*(t+2)/2)];
 					t_inc++;
 					t+=t_inc;
 				}
 			}
-		}*/
-
-
-		/*for(i=0; i<5; i++){
-			for(j=0; j<5; j++){
-				for(k=0; k<6; k++){
-					state[i][j][k]=state[j][3*i+j][k];
-				}
-			}
-		}*/
+		}
 
 		for(i=0; i<5; i++){
 			for(j=0; j<5; j++){
-				for(k=0; k<6; k++){
+				state[i][j][0]=state[j][3*i+j][0];
+			}
+		}
+
+		for(i=0; i<5; i++){
+			for(j=0; j<5; j++){
+				for(k=0; k<8; k++){
 					state[i][j][k]^=((!state[i][j+1][k])&&state[i][j+2][k]);
 				}
 			}
+		}
+
+		for(k=0; k<8; k++){
+			state[0][0][k]^=RC[r];
 		}
 	}
 
 	for(i=0; i<5; i++){
 		for(j=0; j<5; j++){
-			for(k=0; k<6; k++){
-				data[(5*i+j)*6+k]=state[i][j][k];
+			for(k=0; k<8; k++){
+				data[(i*5+j)*8+k]=state[i][j][k];
 			}
 		}
 	}
+}
+
+unsigned int parity(unsigned int n){
+	unsigned int parity=0;
+	while(n){
+		parity=!parity;
+		n=n&(n-1);
+	}
+	return parity;
 }
