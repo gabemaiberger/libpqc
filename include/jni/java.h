bb94591c081e45b95099e5b5ce461e67ebeddab7
Gabe Maiberger <gabrielmaiberger@gmail.com>
2017-11-24 12:37:47 -0700

    Changes to JNI functions

--- include/jni/java.h
+++ include/jni/java.h
@@ -10,82 +10,82 @@ extern "C" {
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_encrypt_ecb
- * Signature: ([Z[Z[Z)V
+ * Signature: ([B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ecb
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_decrypt_ecb
- * Signature: ([Z[Z[Z)V
+ * Signature: ([B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ecb
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_encrypt_ctr
- * Signature: ([Z[Z[Z[Z)V
+ * Signature: ([B[B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ctr
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_decrypt_ctr
- * Signature: ([Z[Z[Z[Z)V
+ * Signature: ([B[B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ctr
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_encrypt_ctr_mt
- * Signature: ([Z[Z[Z[ZI)V
+ * Signature: ([B[B[B[BI)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ctr_1mt
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jbooleanArray, jint);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jbyteArray, jint);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_decrypt_ctr_mt
- * Signature: ([Z[Z[Z[ZI)V
+ * Signature: ([B[B[B[BI)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ctr_1mt
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jbooleanArray, jint);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jbyteArray, jint);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_encrypt_xex
- * Signature: ([Z[Z[Z)V
+ * Signature: ([B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1xex
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_decrypt_xex
- * Signature: ([Z[Z[Z)V
+ * Signature: ([B[B[B)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1xex
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_encrypt_xex_mt
- * Signature: ([Z[Z[ZI)V
+ * Signature: ([B[B[BI)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1xex_1mt
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jint);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jint);
 
 /*
  * Class:     gabe_cryptography_LibPQC
  * Method:    r3d_decrypt_xex_mt
- * Signature: ([Z[Z[ZI)V
+ * Signature: ([B[B[BI)V
  */
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1xex_1mt
-  (JNIEnv *, jobject, jbooleanArray, jbooleanArray, jbooleanArray, jint);
+  (JNIEnv *, jobject, jbyteArray, jbyteArray, jbyteArray, jint);
 
 #ifdef __cplusplus
 }
--- java.c
+++ java.c
@@ -1,56 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <jni/java.h>
 
 #include <r3d.h>
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ecb
- (JNIEnv *env, jobject obj, jbooleanArray plaintext, jbooleanArray key, jbooleanArray ciphertext){
+ (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray ciphertext){
 	int size=sizeof(&plaintext)/sizeof((&plaintext)[0]);
-	r3d_encrypt_ecb((unsigned char *)plaintext, (unsigned char *)key, (unsigned char *)ciphertext, size);
+
+	unsigned char *plaintext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *ciphertext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, size, (jbyte *)key_jni);
+
+	r3d_encrypt_ecb(plaintext_jni, key_jni, ciphertext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ecb
-  (JNIEnv *env, jobject obj, jbooleanArray ciphertext, jbooleanArray key, jbooleanArray plaintext){
+  (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray plaintext){
+	int size=sizeof(&ciphertext)/sizeof((&ciphertext)[0]);
+
+	unsigned char *ciphertext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *plaintext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, size, (jbyte *)key_jni);
+
+	r3d_decrypt_ecb(ciphertext_jni, key_jni, plaintext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ctr
-  (JNIEnv *env, jobject obj, jbooleanArray plaintext, jbooleanArray key, jbooleanArray iv, jbooleanArray ciphertext){
+  (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray iv, jbyteArray ciphertext){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ctr
-  (JNIEnv *env, jobject obj, jbooleanArray ciphertext, jbooleanArray key, jbooleanArray iv, jbooleanArray plaintext){
+  (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray iv, jbyteArray plaintext){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1ctr_1mt
-  (JNIEnv *env, jobject obj, jbooleanArray plaintext, jbooleanArray key, jbooleanArray iv, jbooleanArray ciphertext, jint num_threads){
+  (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray iv, jbyteArray ciphertext, jint num_threads){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1ctr_1mt
-  (JNIEnv *env, jobject obj, jbooleanArray ciphertext, jbooleanArray key, jbooleanArray iv, jbooleanArray plaintext, jint num_threads){
+  (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray iv, jbyteArray plaintext, jint num_threads){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1xex
-  (JNIEnv *env, jobject obj, jbooleanArray plaintext, jbooleanArray key, jbooleanArray ciphertext){
+  (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray ciphertext){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1xex
-  (JNIEnv *env, jobject obj, jbooleanArray ciphertext, jbooleanArray key, jbooleanArray plaintext){
+  (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray plaintext){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1encrypt_1xex_1mt
-  (JNIEnv *env, jobject obj, jbooleanArray plaintext, jbooleanArray key, jbooleanArray ciphertext, jint num_threads){
+  (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray ciphertext, jint num_threads){
 	return;
 }
 
 JNIEXPORT void JNICALL Java_gabe_cryptography_LibPQC_r3d_1decrypt_1xex_1mt
-  (JNIEnv *env, jobject obj, jbooleanArray ciphertext, jbooleanArray key, jbooleanArray plaintext, jint num_threads){
+  (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray plaintext, jint num_threads){
 	return;
 }
