fc06119546c28d74f3ed1caf5b1c238ab382a229
Gabe Maiberger <gabrielmaiberger@gmail.com>
2018-05-13 13:03:20 -0600

    Add rest of encryption modes to JNI

--- java.c
+++ java.c
@@ -32,7 +32,7 @@ JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1encrypt_1ecb
 	unsigned char *ciphertext_jni=malloc(size);
 
 	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
-	(*env)->GetByteArrayRegion(env, key, 0, size, (jbyte *)key_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
 
 	r3d_encrypt_ecb(plaintext_jni, key_jni, ciphertext_jni, size);
 
@@ -50,7 +50,7 @@ JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1decrypt_1ecb
 	unsigned char *plaintext_jni=malloc(size);
 
 	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
-	(*env)->GetByteArrayRegion(env, key, 0, size, (jbyte *)key_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
 
 	r3d_decrypt_ecb(ciphertext_jni, key_jni, plaintext_jni, size);
 
@@ -61,40 +61,150 @@ JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1decrypt_1ecb
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1encrypt_1ctr
   (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray iv, jbyteArray ciphertext){
+	int size=(sizeof(&plaintext)/sizeof((&plaintext)[0]))+((sizeof(&plaintext)/sizeof((&plaintext)[0]))%512);
+
+	unsigned char *plaintext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *iv_jni=malloc(512);
+	unsigned char *ciphertext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+	(*env)->GetByteArrayRegion(env, iv, 0, 512, (jbyte *)key_jni);
+
+	r3d_encrypt_ctr(plaintext_jni, key_jni, iv_jni, ciphertext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1decrypt_1ctr
   (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray iv, jbyteArray plaintext){
+	int size=(sizeof(&ciphertext)/sizeof((&ciphertext)[0]))+((sizeof(&ciphertext)/sizeof((&ciphertext)[0]))%512);
+
+	unsigned char *ciphertext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *iv_jni=malloc(512);
+	unsigned char *plaintext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_decrypt_ctr(ciphertext_jni, key_jni, iv_jni, plaintext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1encrypt_1ctr_1mt
   (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray iv, jbyteArray ciphertext, jint num_threads){
+	int size=(sizeof(&plaintext)/sizeof((&plaintext)[0]))+((sizeof(&plaintext)/sizeof((&plaintext)[0]))%512);
+
+	unsigned char *plaintext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *iv_jni=malloc(512);
+	unsigned char *ciphertext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+	(*env)->GetByteArrayRegion(env, iv, 0, 512, (jbyte *)key_jni);
+
+	r3d_encrypt_ctr_mt(plaintext_jni, key_jni, iv_jni, ciphertext_jni, size, num_threads);
+
+	(*env)->SetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_r3d_1decrypt_1ctr_1mt
   (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray iv, jbyteArray plaintext, jint num_threads){
+	int size=(sizeof(&ciphertext)/sizeof((&ciphertext)[0]))+((sizeof(&ciphertext)/sizeof((&ciphertext)[0]))%512);
+
+	unsigned char *ciphertext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *iv_jni=malloc(512);
+	unsigned char *plaintext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_decrypt_ctr_mt(ciphertext_jni, key_jni, iv_jni, plaintext_jni, size, num_threads);
+
+	(*env)->SetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1encrypt_1xex
   (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray ciphertext){
+	int size=(sizeof(&plaintext)/sizeof((&plaintext)[0]))+((sizeof(&plaintext)/sizeof((&plaintext)[0]))%512);
+
+	unsigned char *plaintext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *ciphertext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_encrypt_xex(plaintext_jni, key_jni, ciphertext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1decrypt_1xex
   (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray plaintext){
+	int size=(sizeof(&ciphertext)/sizeof((&ciphertext)[0]))+((sizeof(&ciphertext)/sizeof((&ciphertext)[0]))%512);
+
+	unsigned char *ciphertext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *plaintext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_decrypt_xex(ciphertext_jni, key_jni, plaintext_jni, size);
+
+	(*env)->SetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1encrypt_1xex_1mt
   (JNIEnv *env, jobject obj, jbyteArray plaintext, jbyteArray key, jbyteArray ciphertext, jint num_threads){
+	int size=(sizeof(&plaintext)/sizeof((&plaintext)[0]))+((sizeof(&plaintext)/sizeof((&plaintext)[0]))%512);
+
+	unsigned char *plaintext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *ciphertext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_encrypt_xex_mt(plaintext_jni, key_jni, ciphertext_jni, size, num_threads);
+
+	(*env)->SetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+
 	return;
 }
 
 JNIEXPORT void JNICALL Java_LibPQC_LibPQC_r3d_1decrypt_1xex_1mt
   (JNIEnv *env, jobject obj, jbyteArray ciphertext, jbyteArray key, jbyteArray plaintext, jint num_threads){
+	int size=(sizeof(&ciphertext)/sizeof((&ciphertext)[0]))+((sizeof(&ciphertext)/sizeof((&ciphertext)[0]))%512);
+
+	unsigned char *ciphertext_jni=malloc(size);
+	unsigned char *key_jni=malloc(512);
+	unsigned char *plaintext_jni=malloc(size);
+
+	(*env)->GetByteArrayRegion(env, ciphertext, 0, size, (jbyte *)ciphertext_jni);
+	(*env)->GetByteArrayRegion(env, key, 0, 512, (jbyte *)key_jni);
+
+	r3d_decrypt_xex_mt(ciphertext_jni, key_jni, plaintext_jni, size, num_threads);
+
+	(*env)->SetByteArrayRegion(env, plaintext, 0, size, (jbyte *)plaintext_jni);	
+
 	return;
 }
